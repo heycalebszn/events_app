fbe01f033ebb9af49259da4295f9412fbecf7b2a
thecodingmontana <dev.humblechris254@gmail.com>
2025-01-05 12:41:21 +0300

    fix(app): rate-limiting failed to build in production

--- actions/webauthn.ts
+++ actions/webauthn.ts
@@ -1,18 +1,14 @@
 "use server";
-
-import { RefillingTokenBucket } from "@/lib/server/rate-limit";
 import { createWebAuthnChallenge } from "@/lib/server/webauthn";
 import { encodeBase64 } from "@oslojs/encoding";
-import { headers } from "next/headers";
-
-const webauthnChallengeRateLimitBucket = new RefillingTokenBucket<string>(30, 10);
+// import { headers } from "next/headers";
 
 export async function createWebAuthnChallengeAction(): Promise<string> {
-	console.log("create");
-	const clientIP = (await headers()).get("X-Forwarded-For");
-	if (clientIP !== null && !webauthnChallengeRateLimitBucket.consume(clientIP, 1)) {
-		throw new Error("Too many requests");
-	}
-	const challenge = createWebAuthnChallenge();
-	return encodeBase64(challenge);
+  // todo: add user rate-limit here
+  // const clientIP = (await headers()).get("X-Forwarded-For");
+  // if (clientIP !== null && !webauthnChallengeRateLimitBucket.consume(clientIP, 1)) {
+  // 	throw new Error("Too many requests");
+  // }
+  const challenge = createWebAuthnChallenge();
+  return encodeBase64(challenge);
 }
