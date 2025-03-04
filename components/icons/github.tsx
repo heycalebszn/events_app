4717253cc82bbd462707e46544e8919e21fb49c4
olivebishop <olivehendrilgen1@gmail.com>
2025-03-04 05:48:46 +0300

    fix(login auth pages): revamp auth login page

--- components/icons/github.tsx
+++ components/icons/github.tsx
@@ -1,100 +1,18 @@
-'use client';
+// This file would be placed at: @/components/icons/github.tsx
+import React from "react";
 
-import type { Variants } from 'motion/react';
-import { motion, useAnimation } from 'motion/react';
-
-const bodyVariants: Variants = {
-  normal: {
-    opacity: 1,
-    pathLength: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  animate: {
-    opacity: [0, 1],
-    pathLength: [0, 1],
-    scale: [0.9, 1],
-    transition: {
-      duration: 0.4,
-    },
-  },
-};
-
-const tailVariants: Variants = {
-  normal: {
-    pathLength: 1,
-    rotate: 0,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  draw: {
-    pathLength: [0, 1],
-    rotate: 0,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  wag: {
-    pathLength: 1,
-    rotate: [0, -15, 15, -10, 10, -5, 5],
-    transition: {
-      duration: 2.5,
-      ease: 'easeInOut',
-      repeat: Infinity,
-    },
-  },
-};
-
-const GithubIcon = () => {
-  const bodyControls = useAnimation();
-  const tailControls = useAnimation();
-
-  const handleMouseEnter = async () => {
-    bodyControls.start('animate');
-    await tailControls.start('draw');
-    tailControls.start('wag');
-  };
-
-  const handleMouseLeave = () => {
-    bodyControls.start('normal');
-    tailControls.start('normal');
-  };
-
+export function GithubIcon() {
   return (
-    <div
-      className="cursor-pointer select-none p-2 hover:bg-accent rounded-md transition-colors duration-200 flex items-center justify-center"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="currentColor"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <motion.path
-          variants={bodyVariants}
-          initial="normal"
-          animate={bodyControls}
-          d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-        />
-        <motion.path
-          variants={tailVariants}
-          initial="normal"
-          animate={tailControls}
-          d="M9 18c-4.51 2-5-2-7-2"
-        />
-      </svg>
-    </div>
+      <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.342-3.369-1.342-.454-1.155-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.268 2.75 1.026A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.026 2.747-1.026.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.841-2.337 4.687-4.565 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.161 22 16.417 22 12c0-5.523-4.477-10-10-10z" />
+    </svg>
   );
-};
+}
 
-export { GithubIcon };
+export default GithubIcon;
